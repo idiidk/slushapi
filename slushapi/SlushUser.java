d49f165e344dd9d445f0a2fc085087dff856e040
Vincent Brouwer <kipsensatie@gmail.com>
2016-07-03 23:07:38 +0200

    Made a rough first API.

---
+++ slushapi/SlushUser.java
@@ -0,0 +1,28 @@
+package com.slushapi;
+
+/**
+ * Created by Kipsensatie on 3-7-2016.
+ */
+public class SlushUser {
+
+    public static String username = "";
+    public static String wallet = "";
+    public static String hashrate = "";
+    public static String send_threshold = "";
+    public static String send_threshold_usd = "";
+
+    public static String BTCUSD = "";
+    public static String wallet_balance = "";
+    public static String wallet_balance_usd = "";
+
+    public static String confirmed_reward = "";
+    public static String unconfirmed_reward = "";
+    public static String estimated_reward = "";
+    public static String total_reward = "";
+
+    public static String confirmed_reward_usd = "";
+    public static String unconfirmed_reward_usd = "";
+    public static String estimated_reward_usd = "";
+    public static String total_reward_usd = "";
+
+}
