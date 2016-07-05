e5d410b0f398850a19260fc14c87a122bb92318e
Vincent Brouwer <kipsensatie@gmail.com>
2016-07-05 13:04:59 +0200

    Total Recode
    Fixed bug where it wouldn't initialize the user class #3.

--- src/com/kipsensatie/slushapi/SlushUser.java
+++ src/com/kipsensatie/slushapi/SlushUser.java
@@ -1,28 +1,85 @@
 package com.kipsensatie.slushapi;
 
+import com.google.gson.JsonObject;
+
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+
 /**
- * Created by Kipsensatie on 3-7-2016.
+ * Created by Kipsensatie on 5-7-2016.
  */
 public class SlushUser {
+    private String ApiKey;
+    private String ApiUrl;
+    private SlushApi API;
 
-    public static String username = "";
-    public static String wallet = "";
-    public static String hashrate = "";
-    public static String send_threshold = "";
-    public static String send_threshold_usd = "";
+    public String getUsername() {
+        return username;
+    }
 
-    public static String BTCUSD = "";
-    public static String wallet_balance = "";
-    public static String wallet_balance_usd = "";
+    public String getWallet() {
+        return wallet;
+    }
 
-    public static String confirmed_reward = "";
-    public static String unconfirmed_reward = "";
-    public static String estimated_reward = "";
-    public static String total_reward = "";
+    public String getHashrate() {
+        return hashrate;
+    }
 
-    public static String confirmed_reward_usd = "";
-    public static String unconfirmed_reward_usd = "";
-    public static String estimated_reward_usd = "";
-    public static String total_reward_usd = "";
+    public String getSendThreshold() {
+        return send_threshold;
+    }
 
+    public String getConfirmedReward() {
+        return confirmed_reward;
+    }
+
+    public String getUnconfirmedReward() {
+        return unconfirmed_reward;
+    }
+
+    public String getEstimatedReward() {
+        return estimated_reward;
+    }
+
+    public String getTotalReward() {
+        return total_reward;
+    }
+
+    private String username = "";
+    private String wallet = "";
+    private String hashrate = "";
+    private String send_threshold = "";
+    private String confirmed_reward = "";
+    private String unconfirmed_reward = "";
+    private String estimated_reward = "";
+    private String total_reward = "";
+    
+    public SlushUser(SlushApi slushApi) {
+        API = slushApi;
+    }
+
+
+
+    public boolean getUserData() {
+        JsonObject slushObj;
+        if (API == null) {
+            return false;
+        } else {
+            try {
+                slushObj = API.getJsonObject();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+                return false;
+            }
+            username = slushObj.get("username").getAsString();
+            wallet = slushObj.get("wallet").getAsString();
+            hashrate = slushObj.get("hashrate").getAsString();
+            send_threshold = slushObj.get("send_threshold").getAsString();
+            unconfirmed_reward = slushObj.get("unconfirmed_reward").getAsString();
+            confirmed_reward = slushObj.get("confirmed_reward").getAsString();
+            estimated_reward = slushObj.get("estimated_reward").getAsString();
+            total_reward = Double.toString(Double.parseDouble(unconfirmed_reward) + Double.parseDouble(confirmed_reward));
+        }
+        return true;
+    }
 }
